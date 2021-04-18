d2ac1c6ddbb3d661267142bfc23544c8aa012947
admin123 <akashgooljar07@gmail.com>
2021-04-18 18:33:08 +0400

    [dev]-Adding Hint Mode

---
+++ bin/CMakeFiles/smain.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/smain.dir/src/dico.c.o"
+  "CMakeFiles/smain.dir/src/main.c.o"
+  "smain"
+  "smain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/smain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
